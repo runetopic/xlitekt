f26200a906f80fee6df3365039efde47bc4dd2c5
Tyler Telis <xlitersps@gmail.com>
2022-01-24 18:42:38 -0500

    Convert Boolean ext to expression body

--- src/main/kotlin/com/runetopic/xlitekt/util/ext/BooleanExt.kt
+++ src/main/kotlin/com/runetopic/xlitekt/util/ext/BooleanExt.kt
@@ -3,10 +3,5 @@ package com.runetopic.xlitekt.util.ext
 /**
  * @author Jordan Abraham
  */
-fun Boolean.toInt(): Int {
-    return if (this) 1 else 0
-}
-
-fun Boolean.toIntInv(): Int {
-    return if (this) 0 else 1
-}
+fun Boolean.toInt(): Int = if (this) 1 else 0
+fun Boolean.toIntInv(): Int = if (this) 0 else 1
