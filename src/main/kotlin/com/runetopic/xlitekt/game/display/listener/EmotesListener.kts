739e41e3a95c21a1705f3eb91543b488cca8f52a
Tyler Telis <xlitersps@gmail.com>
2022-02-08 15:59:30 -0500

    Fixing enum naming convention

--- src/main/kotlin/com/runetopic/xlitekt/game/display/listener/EmotesListener.kts
+++ src/main/kotlin/com/runetopic/xlitekt/game/display/listener/EmotesListener.kts
@@ -1,11 +1,11 @@
 package com.runetopic.xlitekt.game.display.listener
 
-import com.runetopic.xlitekt.game.display.InterfaceEvent.ClickOp1
+import com.runetopic.xlitekt.game.display.InterfaceEvent.ClICK_OPTION_1
 import com.runetopic.xlitekt.game.display.InterfaceId
 import com.runetopic.xlitekt.game.display.InterfaceListener.Companion.buildInterfaceListener
 
 buildInterfaceListener(InterfaceId.EMOTES) {
     onOpen {
-        event(childId = 1, slots = 0..50, events = ClickOp1)
+        event(childId = 1, slots = 0..50, events = ClICK_OPTION_1)
     }
 }
