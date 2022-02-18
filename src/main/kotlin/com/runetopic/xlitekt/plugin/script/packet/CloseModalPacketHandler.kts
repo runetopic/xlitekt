c6be933ceed89bc1389a8a89eaf4c42809ade9dc
Tyler Telis <xlitersps@gmail.com>
2022-02-18 13:50:17 -0500

    Closing support for extended interfaces

--- src/main/kotlin/com/runetopic/xlitekt/plugin/script/packet/CloseModalPacketHandler.kts
+++ src/main/kotlin/com/runetopic/xlitekt/plugin/script/packet/CloseModalPacketHandler.kts
@@ -1,5 +1,6 @@
 package com.runetopic.xlitekt.plugin.script.packet
 
+import com.runetopic.xlitekt.game.actor.player.message
 import com.runetopic.xlitekt.network.packet.CloseModalPacket
 import com.runetopic.xlitekt.network.packet.disassembler.handler.onPacket
 
@@ -8,4 +9,5 @@ import com.runetopic.xlitekt.network.packet.disassembler.handler.onPacket
  */
 onPacket<CloseModalPacket> {
     player.interfaces.closeModal()
+    player.message("Closing")
 }
--- src/main/kotlin/com/runetopic/xlitekt/plugin/script/ui/AdvancedSettingsListener.kts
+++ src/main/kotlin/com/runetopic/xlitekt/plugin/script/ui/AdvancedSettingsListener.kts
@@ -1,13 +1,29 @@
 package com.runetopic.xlitekt.plugin.script.ui
 
+import com.runetopic.xlitekt.game.ui.InterfaceEvent
 import com.runetopic.xlitekt.game.ui.UserInterface
 import com.runetopic.xlitekt.game.ui.onInterface
+
+
+private val closeSettingsChildId = 4
 
 /**
  * @author Jordan Abraham
  */
 onInterface<UserInterface.AdvancedSettings> {
+    onInit {
+        // Varbit(id = 9657, value = 0)
+        // Varbit(id = 9656, value = 4)
+        // Varbit(id = 9658, value = 1)
+    }
     onOpen {
-        // TODO Grab the ifEvents from RuneScape.
+        setEvent(21, 0..147, InterfaceEvent.CLICK_OPTION_1)
+        setEvent(23, 0..7, InterfaceEvent.CLICK_OPTION_1)
+        setEvent(19, 0..240, InterfaceEvent.CLICK_OPTION_1)
+        setEvent(28, 0..122, InterfaceEvent.CLICK_OPTION_1)
+    }
+
+    onClick(childId = closeSettingsChildId) {
+        interfaces -= UserInterface.AdvancedSettings
     }
 }
