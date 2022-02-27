f24ca1ed0ca12cb2e6e56a76bb3a7e866afd4265
Jordan <personal.jordanabraham@gmail.com>
2022-02-27 01:55:18 -0500

    Add fix to packet disassemblers.

--- src/main/kotlin/com/runetopic/xlitekt/network/packet/disassembler/PacketDisassemblerListener.kt
+++ src/main/kotlin/com/runetopic/xlitekt/network/packet/disassembler/PacketDisassemblerListener.kt
@@ -2,15 +2,14 @@ package com.runetopic.xlitekt.network.packet.disassembler
 
 import com.runetopic.xlitekt.network.packet.Packet
 import io.ktor.utils.io.core.ByteReadPacket
-import kotlin.reflect.KClass
 
 /**
  * @author Jordan Abraham
  */
 object PacketDisassemblerListener {
-    val listeners = mutableMapOf<KClass<*>, PacketDisassembler>()
+    val listeners = mutableMapOf<Int, PacketDisassembler>()
 }
 
 inline fun <reified T : Packet> onPacketDisassembler(opcode: Int, size: Int, noinline packet: ByteReadPacket.() -> T) {
-    PacketDisassemblerListener.listeners[T::class] = PacketDisassembler(opcode, size, packet)
+    PacketDisassemblerListener.listeners[opcode] = PacketDisassembler(opcode, size, packet)
 }
