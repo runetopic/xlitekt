6d5845a179f0294900fb63a70d6bb5990e25f76a
Jordan <personal.jordanabraham@gmail.com>
2022-03-19 06:49:43 -0400

    Update PlayerSynchronizer.

--- game/src/main/kotlin/xlitekt/game/world/engine/sync/PlayerSynchronizer.kt
+++ game/src/main/kotlin/xlitekt/game/world/engine/sync/PlayerSynchronizer.kt
@@ -1,14 +1,13 @@
 package xlitekt.game.world.engine.sync
 
 import com.github.michaelbull.logging.InlineLogger
-import org.rsmod.pathfinder.Route
 import io.ktor.utils.io.core.ByteReadPacket
-import java.util.concurrent.ConcurrentHashMap
-import kotlin.random.Random.Default.nextInt
 import kotlinx.coroutines.Runnable
+import org.rsmod.pathfinder.Route
 import org.rsmod.pathfinder.SmartPathFinder
 import org.rsmod.pathfinder.ZoneFlags
 import xlitekt.game.actor.movement.MovementStep
+import xlitekt.game.actor.movement.isValid
 import xlitekt.game.actor.player.Player
 import xlitekt.game.actor.player.sendRebuildNormal
 import xlitekt.game.actor.player.shouldRebuildMap
@@ -18,9 +17,9 @@ import xlitekt.game.packet.PlayerInfoPacket
 import xlitekt.game.world.World
 import xlitekt.game.world.map.location.Location
 import xlitekt.shared.inject
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.random.Random.Default.nextInt
 import kotlin.time.measureTime
-import xlitekt.game.actor.movement.emptyStep
-import xlitekt.game.actor.movement.isValid
 
 /**
  * @author Jordan Abraham
@@ -67,20 +66,23 @@ class PlayerSynchronizer : Runnable {
                     }
                 }
 
+                // Pre player process.
                 val steps = ConcurrentHashMap<Player, MovementStep>()
-                // Move the player.
-                players.parallelStream().forEach { steps[it] = it.processMovement() }
-                // Collect player locations.
-                val locations = players.associateWith(Player::location)
-                // Collect player previous locations.
-                val previousLocations = players.associateWith(Player::previousLocation)
-                // Collect player pending block updates.
-                val pending = players.associateWith(Player::pendingUpdates)
                 val updates = ConcurrentHashMap<Player, ByteReadPacket>()
-                // Build the player pending block updates.
-                players.parallelStream().forEach { updates[it] = it.processUpdateBlocks(pending) }
-                // Sync players.
-                players.parallelStream().forEach { it.processSync(updates, previousLocations, locations, steps) }
+                players.parallelStream().forEach {
+                    steps[it] = it.processMovement()
+                    updates[it] = it.processUpdateBlocks(it.pendingUpdates())
+                }
+
+                // Main player process.
+                players.parallelStream().forEach {
+                    it.processSync(
+                        updates = updates,
+                        previousLocations = players.associateWith(Player::previousLocation),
+                        locations = players.associateWith(Player::location),
+                        steps = steps
+                    )
+                }
                 tick++
             }
             logger.debug { "Tick took $start for ${players.size} players. [TICK=$tick]" }
@@ -93,10 +95,9 @@ class PlayerSynchronizer : Runnable {
         if (it.isValid() && shouldRebuildMap()) sendRebuildNormal(false)
     }
 
-    private fun Player.processUpdateBlocks(pending: Map<Player, List<Render>>): ByteReadPacket {
-        val renders = pending[this] ?: return ByteReadPacket.Empty
-        if (renders.isEmpty()) return ByteReadPacket.Empty
-        return renders.buildPlayerUpdateBlocks(this)
+    private fun Player.processUpdateBlocks(pending: List<Render>): ByteReadPacket {
+        if (pending.isEmpty()) return ByteReadPacket.Empty
+        return pending.buildPlayerUpdateBlocks(this)
     }
     private fun Player.processSync(updates: Map<Player, ByteReadPacket>, previousLocations: Map<Player, Location?>, locations: Map<Player, Location>, steps: Map<Player, MovementStep?>) {
         write(PlayerInfoPacket(viewport, updates, previousLocations, locations, steps))
