0ba57716bce860d9a1a4657bd7e83338b8dccf2d
Jordan <personal.jordanabraham@gmail.com>
2022-05-05 01:21:16 -0400

    Update MusicPlayer tool.

--- cache/src/main/kotlin/xlitekt/cache/tool/MusicPlayer.kt
+++ cache/src/main/kotlin/xlitekt/cache/tool/MusicPlayer.kt
@@ -7,9 +7,14 @@ import xlitekt.cache.provider.music.MusicEntryTypeProvider
 import xlitekt.shared.inject
 import java.io.ByteArrayInputStream
 import java.nio.file.Path
+import java.sql.Time
 import java.util.Scanner
+import java.util.concurrent.TimeUnit
 import javax.sound.midi.MidiSystem
-import kotlin.system.exitProcess
+import javax.sound.midi.Sequence
+import javax.sound.midi.Sequencer
+import javax.sound.midi.Synthesizer
+import kotlin.io.path.readBytes
 
 /**
  * @author Jordan Abraham
@@ -18,42 +23,60 @@ fun main() {
     startKoin {
         loadKoinModules(cacheModule)
     }
+    MusicPlayer.start()
+}
 
-    val musics by inject<MusicEntryTypeProvider>()
-    var trackId = -1
-    val sequencer = MidiSystem.getSequencer()
+internal object MusicPlayer {
 
-    val scanner = Scanner(System.`in`)
-    while (true) {
-        if (trackId == -1) {
-            println("Enter track id...")
-            trackId = scanner.nextInt()
-            val entry = musics.entryType(trackId) ?: run {
+    fun start() {
+        val soundBank = MidiSystem.getSoundbank(ByteArrayInputStream(Path.of("./cache/data/dump/soundbank/soundbank.sf2").readBytes()))
+
+        val musics by inject<MusicEntryTypeProvider>()
+        var sequence: Sequence?
+        var sequencers: Array<Sequencer?>? = null
+        var synthesizers: Array<Synthesizer?>? = null
+
+        val scanner = Scanner(System.`in`)
+        while (true) {
+            println("Enter track ID (0-${musics.size()})")
+            val entry = musics.entryType(scanner.nextInt()) ?: run {
                 println("Not found!")
-                exitProcess(0)
+                return
             }
-            val synthesizer = MidiSystem.getSynthesizer()
-            synthesizer.open()
-            synthesizer.unloadAllInstruments(synthesizer.defaultSoundbank)
-            synthesizer.loadAllInstruments(MidiSystem.getSoundbank(Path.of("./cache/data/dump/soundbank/soundbank.sf2").toFile()))
 
-            sequencer.open()
-            sequencer.transmitter.receiver = synthesizer.receiver
-            sequencer.sequence = MidiSystem.getSequence(ByteArrayInputStream(entry.bytes!!))
+            println("Loading...")
 
-            Thread.sleep(1000L)
-            sequencer.start()
-        } else {
-            println("Enter next track id...")
-            trackId = scanner.nextInt()
-            val entry = musics.entryType(trackId) ?: run {
-                println("Not found!")
-                exitProcess(0)
+            sequencers?.onEach { it?.stop() }
+            synthesizers?.onEach { it?.close() }
+
+            sequence = MidiSystem.getSequence(ByteArrayInputStream(entry.bytes!!))
+            val count = sequence.tracks.size
+            sequencers = arrayOfNulls(count)
+            synthesizers = arrayOfNulls(count)
+
+            repeat(count) {
+                synthesizers[it] = MidiSystem.getSynthesizer()
+                synthesizers[it]!!.unloadAllInstruments(synthesizers[it]!!.defaultSoundbank)
+                synthesizers[it]!!.loadAllInstruments(soundBank)
+                synthesizers[it]!!.open()
             }
-            sequencer.stop()
-            sequencer.sequence = MidiSystem.getSequence(ByteArrayInputStream(entry.bytes!!))
-            Thread.sleep(1000L)
-            sequencer.start()
+
+            repeat(count) {
+                sequencers[it] = MidiSystem.getSequencer(false)
+                sequencers[it]!!.transmitter.receiver = synthesizers[it]!!.receiver
+                sequencers[it]!!.sequence = sequence
+                sequencers[it]!!.setTrackSolo(it, true)
+                sequencers[it]!!.open()
+            }
+
+            Thread.sleep(1000)
+
+            repeat(sequencers.size) {
+                sequencers[it]!!.start()
+            }
+            val minutes = TimeUnit.MICROSECONDS.toMinutes(sequence!!.microsecondLength)
+            val seconds = TimeUnit.MICROSECONDS.toSeconds(sequence.microsecondLength) % 60
+            println("Playing ${entry.name} --- $minutes minutes and $seconds seconds long.")
         }
     }
 }
