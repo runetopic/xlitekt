81c1275057144c71313f17789ee56c8b361c769e
Jordan <personal.jordanabraham@gmail.com>
2022-05-31 00:31:33 -0400

    Add missing author tags.

--- script/src/main/kotlin/script/ui/CombatOptionsListener.kts
+++ script/src/main/kotlin/script/ui/CombatOptionsListener.kts
@@ -4,6 +4,9 @@ import xlitekt.game.content.ui.UserInterface.CombatOptions
 import xlitekt.game.content.ui.onInterface
 import xlitekt.game.content.vars.VarPlayer
 
+/**
+ * @author Tyler Telis
+ */
 onInterface<CombatOptions> {
     onCreate {
         vars[VarPlayer.SpecialAttackEnergy] = 100 * 10
--- script/src/main/kotlin/script/ui/SkillsListener.kts
+++ script/src/main/kotlin/script/ui/SkillsListener.kts
@@ -2,6 +2,9 @@ import xlitekt.game.actor.player.message
 import xlitekt.game.content.ui.UserInterface
 import xlitekt.game.content.ui.onInterface
 
+/**
+ * @author Justin Kenney
+ */
 onInterface<UserInterface.Skills> {
     onClick(childId = 1) {
         message { "Clicked on attack" }
