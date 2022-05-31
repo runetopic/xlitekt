4c235c618d199821ce39dd5968880dd12be68d36
Jordan <personal.jordanabraham@gmail.com>
2022-05-31 00:15:41 -0400

    Add Projectile class.

---
+++ game/src/main/kotlin/xlitekt/game/content/projectile/Projectile.kt
@@ -0,0 +1,20 @@
+package xlitekt.game.content.projectile
+
+import xlitekt.game.world.map.location.Location
+
+/**
+ * @author Jordan Abraham
+ */
+data class Projectile(
+    val id: Int,
+    val startLocation: Location,
+    val endLocation: Location,
+    val startHeight: Int,
+    val endHeight: Int,
+    val delay: Int,
+    val angle: Int,
+    val steepness: Int
+) {
+    fun distanceX() = endLocation.x - startLocation.x
+    fun distanceZ() = endLocation.z - startLocation.z
+}
