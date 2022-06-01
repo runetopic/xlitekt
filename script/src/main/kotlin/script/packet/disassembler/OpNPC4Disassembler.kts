e2d600276c9173b74f13ea4ebf62985167233b90
justin51 <justin.kenney51@gmail.com>
2022-06-01 08:17:40 -0400

    Added OpNPC4Disassembler

---
+++ script/src/main/kotlin/script/packet/disassembler/OpNPC4Disassembler.kts
@@ -0,0 +1,16 @@
+import io.ktor.utils.io.core.*
+import xlitekt.game.packet.OpNPCPacket
+import xlitekt.game.packet.disassembler.onPacketDisassembler
+import xlitekt.shared.buffer.readUByteAdd
+import xlitekt.shared.toBoolean
+
+/**
+ * @author Justin Kenney
+ */
+onPacketDisassembler(opcode = 74, size = 3) {
+    OpNPCPacket(
+        index = 4,
+        npcIndex = readShortLittleEndian().toInt(),
+        isModifiedClick = readUByteAdd().toBoolean()
+    )
+}
