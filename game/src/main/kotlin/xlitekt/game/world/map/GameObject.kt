52d7da9b242c9c4c3badc91107c58732d327c970
Jordan <personal.jordanabraham@gmail.com>
2022-06-04 14:25:53 -0400

    Improve invokeAndClearWritePool performance by only suspending once for IO operation.

---
+++ game/src/main/kotlin/xlitekt/game/world/map/GameObject.kt
@@ -0,0 +1,26 @@
+package xlitekt.game.world.map.obj
+
+import xlitekt.cache.provider.config.loc.LocEntryTypeProvider
+import xlitekt.game.world.map.Location
+import xlitekt.shared.inject
+
+/**
+ * @author Tyler Telis
+ * @author Jordan Abraham
+ */
+data class GameObject(
+    val id: Int,
+    val location: Location,
+    val shape: Int,
+    val rotation: Int,
+    val spawned: Boolean = true
+) {
+    val entry = provider.entryType(id)!!
+    val name = entry.name
+
+    override fun toString(): String = "GameObject(id=$id, location=$location, shape=$shape, rotation=$rotation"
+
+    private companion object {
+        val provider by inject<LocEntryTypeProvider>()
+    }
+}
