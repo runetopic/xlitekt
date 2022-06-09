524068d9f665721d734c0bd3323baee0dff275d9
Jordan <personal.jordanabraham@gmail.com>
2022-06-09 12:22:04 -0400

    Update ByteBufferBuilder class.

--- shared/src/main/kotlin/xlitekt/shared/buffer/ByteBufferBuilder.kt
+++ shared/src/main/kotlin/xlitekt/shared/buffer/ByteBufferBuilder.kt
@@ -13,11 +13,14 @@ inline fun allocate(limit: Int, block: ByteBuffer.() -> Unit): ByteArray = ByteB
     .rewind()
     .moveToByteArray()
 
-inline fun allocateDynamic(limit: Int, block: ByteBuffer.() -> Unit): ByteArray = ByteBuffer.allocate(limit).also(block).run {
-    limit(position())
-    rewind()
-    moveToByteArray()
-}
+inline fun allocateDynamic(limit: Int, block: ByteBuffer.() -> Unit): ByteArray = ByteBuffer
+    .allocate(limit)
+    .also(block)
+    .run {
+        limit(position())
+        rewind()
+        moveToByteArray()
+    }
 
 fun ByteBuffer.writeStringCp1252NullTerminated(value: String) {
     value.toByteArray().forEach(::put)
