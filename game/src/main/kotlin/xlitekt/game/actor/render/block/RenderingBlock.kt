ff9c88eae3ad3049b92be2e0ad64e7560b2b8aaf
Jordan <personal.jordanabraham@gmail.com>
2022-06-10 23:45:32 -0400

    Update RenderingBlock to use buildDynamicPacket function.

--- game/src/main/kotlin/xlitekt/game/actor/render/block/RenderingBlock.kt
+++ game/src/main/kotlin/xlitekt/game/actor/render/block/RenderingBlock.kt
@@ -1,11 +1,10 @@
 package xlitekt.game.actor.render.block
 
 import io.ktor.utils.io.core.BytePacketBuilder
-import io.ktor.utils.io.core.buildPacket
-import io.ktor.utils.io.core.readBytes
 import io.ktor.utils.io.core.writeShortLittleEndian
 import xlitekt.game.actor.player.Player
 import xlitekt.game.actor.render.Render
+import xlitekt.shared.buffer.buildDynamicPacket
 
 /**
  * @author Jordan Abraham
@@ -26,7 +25,7 @@ data class RenderingBlock(
  * Alternative rendering blocks are for rendering blocks that requires the outside player perspective.
  * An example of an alternative rendering block is for hit splat tinting as it requires the check of the outside player varbit.
  */
-internal fun Collection<HighDefinitionRenderingBlock>.invokeHighDefinitionPlayerRenderingBlocks(player: Player) = buildPacket {
+internal fun Collection<HighDefinitionRenderingBlock>.invokeHighDefinitionPlayerRenderingBlocks(player: Player) = buildDynamicPacket {
     writeMask(fold(0) { current, next -> current or next.renderingBlock.mask }.let { if (it > 0xff) it or 0x10 else it })
     associateWith { it.renderingBlock.packet.invoke(it.render) }.forEach {
         player.setLowDefinitionRenderingBlock(it.key, it.value)
@@ -35,46 +34,46 @@ internal fun Collection<HighDefinitionRenderingBlock>.invokeHighDefinitionPlayer
             writeByte(b)
         }
     }
-}.readBytes()
+}
 
 /**
  * Creates a new ByteArray from a collection of LowDefinitionRenderingBlock for players.
  * This is used by the player info packet for building low definition blocks for players.
  */
-internal fun Collection<LowDefinitionRenderingBlock>.invokeLowDefinitionPlayerRenderingBlocks() = buildPacket {
+internal fun Collection<LowDefinitionRenderingBlock>.invokeLowDefinitionPlayerRenderingBlocks() = buildDynamicPacket {
     writeMask(fold(0) { current, next -> current or next.renderingBlock.mask }.let { if (it > 0xff) it or 0x10 else it })
     map(LowDefinitionRenderingBlock::bytes).forEach {
         for (b in it) {
             writeByte(b)
         }
     }
-}.readBytes()
+}
 
 /**
  * Creates a new ByteArray from a collection of AlternativeDefinitionRenderingBlock for players.
  * This is used by the player update for both high and low definition updates that require outside player checks.
  */
-internal fun Collection<AlternativeDefinitionRenderingBlock>.invokeAlternativeDefinitionPlayerRenderingBlocks() = buildPacket {
+internal fun Collection<AlternativeDefinitionRenderingBlock>.invokeAlternativeDefinitionPlayerRenderingBlocks() = buildDynamicPacket {
     writeMask(fold(0) { current, next -> current or next.renderingBlock.mask }.let { if (it > 0xff) it or 0x10 else it })
     map(AlternativeDefinitionRenderingBlock::bytes).forEach {
         for (b in it) {
             writeByte(b)
         }
     }
-}.readBytes()
+}
 
 /**
  * Creates a new ByteArray from a collection of HighDefinitionRenderingBlock for npcs.
  * This is used by the npc update.
  */
-internal fun Collection<HighDefinitionRenderingBlock>.invokeHighDefinitionNPCRenderingBlocks() = buildPacket {
+internal fun Collection<HighDefinitionRenderingBlock>.invokeHighDefinitionNPCRenderingBlocks() = buildDynamicPacket {
     writeMask(fold(0) { current, next -> current or next.renderingBlock.mask }.let { if (it > 0xff) it or 0x4 else it })
     map { it.renderingBlock.packet.invoke(it.render) }.forEach {
         for (b in it) {
             writeByte(b)
         }
     }
-}.readBytes()
+}
 
 private fun BytePacketBuilder.writeMask(mask: Int) {
     if (mask > 0xff) writeShortLittleEndian(mask.toShort()) else writeByte(mask.toByte())
