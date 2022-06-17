3e0d29886f6829e50c5c219af57c24adf030288c
Jordan <personal.jordanabraham@gmail.com>
2022-06-17 11:22:56 -0400

    Update RSA build.gradle.kts.

--- rsa/build.gradle.kts
+++ rsa/build.gradle.kts
@@ -6,34 +6,26 @@ import java.security.spec.RSAPublicKeySpec
 /**
  * @author Jordan Abraham
  */
-abstract class RSAGenTask : DefaultTask() {
-    @TaskAction
-    fun generate() {
-        val factory = KeyFactory.getInstance("RSA")
-        val pair = KeyPairGenerator.getInstance("RSA").also { it.initialize(1024) }.genKeyPair()
-        val server = factory.getKeySpec(pair.private, RSAPrivateKeySpec::class.java)
-        val client = factory.getKeySpec(pair.public, RSAPublicKeySpec::class.java)
+tasks.register("generateRSA") {
+    val factory = KeyFactory.getInstance("RSA")
+    val pair = KeyPairGenerator.getInstance("RSA").also { it.initialize(1024) }.genKeyPair()
+    val server = factory.getKeySpec(pair.private, RSAPrivateKeySpec::class.java)
+    val client = factory.getKeySpec(pair.public, RSAPublicKeySpec::class.java)
 
-        repeat(5) { println() }
-        println("==================== Use the following for the application.conf ====================")
-        println(
-            """
-                rsa {
-                    exponent = "${server.privateExponent}"
-                    modulus = "${server.modulus}"
-                }
-            """.trimIndent()
-        )
-        println()
-        println("==================== Use following for the client ====================")
-        println(
-            """
+    repeat(5) { println() }
+    println(
+        """
+                ==================== Use the following for the application.conf ====================
+            
+                exponent = "${server.privateExponent}"
+                modulus = "${server.modulus}"
+                
+                
+                ==================== Use following for the client ====================
+                
                 exponent = "${client.publicExponent}"
                 modulus = "${client.modulus}"
-            """.trimIndent()
-        )
-        repeat(5) { println() }
-    }
+        """.trimIndent()
+    )
+    repeat(5) { println() }
 }
-
-tasks.register<RSAGenTask>("generateRSA")
