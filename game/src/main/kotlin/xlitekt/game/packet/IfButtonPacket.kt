ed29becc1981a050c2dfb4c20e4d5cac62ea7971
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 19:23:26 -0400

    Document IfButtonPacket data class.

--- game/src/main/kotlin/xlitekt/game/packet/IfButtonPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/IfButtonPacket.kt
@@ -2,6 +2,12 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * Represents an IF_BUTTON client -> server packet.
+ * @property index The index of this packet followed by IF_BUTTON. Examples: IF_BUTTON1, IF_BUTTON2.
+ * @property packedInterface The packed interface that was clicked by the client. (interfaceId shl 16 or childId).
+ * @property slotId The slotId clicked by the client. This defaults to 65535 if there was no container slot clicked.
+ * @property itemId The itemId clicked by the client. This defaults to 65535 if there was no item clicked.
  */
 data class IfButtonPacket(
     val index: Int,
