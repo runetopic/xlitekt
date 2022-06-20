f2ac3049100e9c536ee7785f7b67c0b317e930b8
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 20:08:47 -0400

    Document LocAddPacket.

--- game/src/main/kotlin/xlitekt/game/packet/LocAddPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/LocAddPacket.kt
@@ -2,8 +2,16 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * Represents the LOC_ADD server -> client packet.
+ * This packet is used to add a loc to a zone.
+ *
+ * @property id The id of the loc.
+ * @property shape The shape of the loc.
+ * @property rotation The rotation of the loc.
+ * @property packedOffset The packed offset location of the loc relative to the client player location.
  */
-class LocAddPacket(
+data class LocAddPacket(
     val id: Int,
     val shape: Int,
     val rotation: Int,
