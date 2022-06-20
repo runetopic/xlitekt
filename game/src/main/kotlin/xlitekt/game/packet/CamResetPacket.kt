073b2a8a88a6d2913af017e0569031d2a5095364
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 22:02:34 -0400

    Document CamResetPacket.

--- game/src/main/kotlin/xlitekt/game/packet/CamResetPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/CamResetPacket.kt
@@ -2,5 +2,15 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * <b>Information</b>
+ *
+ * Represents the CAM_RESET server -> client packet.
+ *
+ * This packet is used to reset the client camera position to the default position.
+ *
+ * <b>Assembly Example</b>
+ *
+ * This packet has an empty body and does not require additional information to send to the client.
  */
 class CamResetPacket : Packet
