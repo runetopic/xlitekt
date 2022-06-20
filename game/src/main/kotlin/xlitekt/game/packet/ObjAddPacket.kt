f760cfdab3d1004aef1992c207cdb91f01b0dd6c
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 20:09:30 -0400

    Document ObjAddPacket.

--- game/src/main/kotlin/xlitekt/game/packet/ObjAddPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/ObjAddPacket.kt
@@ -2,8 +2,15 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * Represents the OBJ_ADD server -> client packet.
+ * This packet is used to add an obj to a zone.
+ *
+ * @property id The id of the obj.
+ * @property amount The amount of the obj.
+ * @property packedOffset The packed offset location of the obj relative to the client player location.
  */
-class ObjAddPacket(
+data class ObjAddPacket(
     val id: Int,
     val amount: Int,
     val packedOffset: Int
