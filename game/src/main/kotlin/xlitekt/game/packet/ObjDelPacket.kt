3f3abab119be83ceb387e6a4c5f9c883a3f435f6
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 20:09:40 -0400

    Document ObjDelPacket.

--- game/src/main/kotlin/xlitekt/game/packet/ObjDelPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/ObjDelPacket.kt
@@ -2,8 +2,14 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * Represents the OBJ_DEL server -> client packet.
+ * This packet is used to delete an obj from a zone.
+ *
+ * @property id The id of the obj.
+ * @property packedOffset The packed offset location of the obj relative to the client player location.
  */
-class ObjDelPacket(
+data class ObjDelPacket(
     val id: Int,
     val packedOffset: Int
 ) : Packet
