4dcf2ce8054eabfa6c3a1ef626ab408f79326894
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 23:48:40 -0400

    Document EntryTypeProvider.

--- cache/src/main/kotlin/xlitekt/cache/provider/EntryTypeProvider.kt
+++ cache/src/main/kotlin/xlitekt/cache/provider/EntryTypeProvider.kt
@@ -9,6 +9,10 @@ import xlitekt.shared.inject
 import xlitekt.shared.toBoolean
 import java.nio.ByteBuffer
 
+/**
+ * @author Jordan Abraham
+ * @author Tyler Telis
+ */
 abstract class EntryTypeProvider<R : EntryType> {
     internal val entries by lazy(::load)
     protected val store by inject<Js5Store>()
@@ -16,11 +20,6 @@ abstract class EntryTypeProvider<R : EntryType> {
     protected abstract fun load(): Map<Int, R>
     protected abstract fun ByteBuffer.loadEntryType(type: R): R
     internal open fun postLoadEntryType(type: R) {}
-
-    fun size() = entries.size
-    fun entryType(id: Int): R? = entries[id]
-    fun exists(id: Int): Boolean = entries.containsKey(id)
-    fun entries(): Collection<R> = entries.values
 
     protected fun ByteBuffer.readStringIntParameters(): Map<Int, Any> = buildMap {
         repeat(readUByte()) {
@@ -33,7 +32,59 @@ abstract class EntryTypeProvider<R : EntryType> {
         check(remaining() == 0) { "The remaining buffer is not empty. Remaining=${remaining()}" }
     }
 
+    /**
+     * Folds a String to a RuneScape cache name hash.
+     */
     protected fun String.toNameHash() = fold(0) { hash, next -> next.code + ((hash shl 5) - hash) }
+
+    /**
+     * Returns the total number of [R]s contained within this provider.
+     *
+     * <b>Example</b>
+     *
+     * ```
+     * val provider by inject<ObjEntryTypeProvider>()
+     * val size = provider.size()
+     * ```
+     */
+    fun size() = entries.size
+
+    /**
+     * Returns a [R] with the given id.
+     *
+     * <b>Example</b>
+     *
+     * ```
+     * val provider by inject<ObjEntryTypeProvider>()
+     * val entry = provider.entryType(abyssal_whip_4151)
+     * ```
+     */
+    fun entryType(id: Int) = entries[id]
+
+    /**
+     * Returns if the given id has an associated [R].
+     * This is widely used as a verifier for server logic as it is safe to compare against the cache information.
+     *
+     * <b>Example</b>
+     *
+     * ```
+     * val provider by inject<ObjEntryTypeProvider>()
+     * val exists = provider.exists(abyssal_whip_4151)
+     * ```
+     */
+    fun exists(id: Int) = entries.containsKey(id)
+
+    /**
+     * Returns a [Collection] of [R]s contained within this provider.
+     *
+     * <b>Example</b>
+     *
+     * ```
+     * val provider by inject<ObjEntryTypeProvider>()
+     * val entries = provider.entries()
+     * ```
+     */
+    fun entries() = entries.values
 
     internal companion object {
         // Indexes.
