bf59f2b629fe34c8ad1f822527af43f573c1f50c
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 20:09:11 -0400

    Document MapProjAnimPacket.

--- game/src/main/kotlin/xlitekt/game/packet/MapProjAnimPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/MapProjAnimPacket.kt
@@ -2,8 +2,22 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * Represents the MAP_PROJANIM server -> client packet.
+ * This packet is used to add a mapProjAnim to a zone.
+ *
+ * @property id The id of the mapProjAnim. This is also the same as a normal spot animation.
+ * @property distanceX The difference between the end location and start location on the X axis.
+ * @property distanceZ The difference between the end location and start location on the Z axis.
+ * @property startHeight The starting height.
+ * @property endHeight The ending height.
+ * @property delay The delay in ticks until the client will see this mapProjAnim.
+ * @property lifespan The total lifespan in ticks the client will see this mapProjAnim.
+ * @property angle The angle.
+ * @property steepness The steepness.
+ * @property packedOffset The packed offset location of the mapProjAnim relative to the client player location.
  */
-class MapProjAnimPacket(
+data class MapProjAnimPacket(
     val id: Int,
     val distanceX: Int,
     val distanceZ: Int,
