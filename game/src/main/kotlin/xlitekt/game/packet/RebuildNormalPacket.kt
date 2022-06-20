86a0a916dd800c3997aecb9dc22bb9966901d987
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 21:39:37 -0400

    Update RebuildNormalPacket.

--- game/src/main/kotlin/xlitekt/game/packet/RebuildNormalPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/RebuildNormalPacket.kt
@@ -21,10 +21,10 @@ import xlitekt.game.world.map.Location
  * // If the client is logging into the game world.
  * if (initialize) {
  *      // Switch to bit modification on the received buffer.
- *      writeBits(30, location.packedLocation)
+ *      writeBits(30, location.packedLocation) // Our player location.
  *      for (index in 1 until 2048) {
  *          if (index == ourPlayerIndex) continue
- *          val otherPlayer = players[index]
+ *          val otherPlayer = players[index] // A collection of players sorted by the player index which contains players that are currently online and ones that are not online.
  *          writeBits(18, otherPlayer?.location?.regionLocation ?: 0)
  *      }
  *      // Switch to byte modification on the received buffer.
@@ -44,8 +44,6 @@ import xlitekt.game.world.map.Location
  *
  * for (x in numberOfZonesOnTheXAxis) {
  *      for (z in numberOfZonesOnTheZAxis) {
- *          // The region this zone is inside of.
- *          val regionId = z + (x shl 8)
  *          // Four xtea keys used to decompress the maps data from the cache on the client side.
  *          val keys = intArrayOf(0, 0, 0, 0)
  *          for (key in keys) {
@@ -60,6 +58,11 @@ import xlitekt.game.world.map.Location
  * @see Location.regionLocation
  * @see Location.zoneX
  * @see Location.zoneZ
+ *
+ * @property viewport The viewport of the [Player] which contains all the necessary information for the [PlayerInfoPacket].
+ * @property location The location of the player.
+ * @property initialize True if the client is logging into the game world. False if the client is already inside the game world.
+ * @property players A map of players that are currently logged into the game world.
  */
 data class RebuildNormalPacket(
     val viewport: Viewport,
