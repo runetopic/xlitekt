bb5602c3ff88cd3f3c3602faafb88fb6210feb5c
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 20:09:57 -0400

    Document UpdateZoneFullFollowsPacket.

--- game/src/main/kotlin/xlitekt/game/packet/UpdateZoneFullFollowsPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/UpdateZoneFullFollowsPacket.kt
@@ -2,6 +2,23 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * Represents the UPDATE_ZONE_FULL_FOLLOWS server -> client packet.
+ * This packet is used to clear a specific zone of updates.
+ * The client takes an input X and Z location and loops +8 on both axis to clear
+ * the entire zone of locs and objs.
+ *
+ * for (x in localX + 8) {
+ *      for (z in localZ + 8) {
+ *          // Loops the 8x8 zone and clears each tile of locs and objs.
+ *      }
+ * }
+ *
+ * @see UpdateZonePartialFollowsPacket To send a single update to a zone after clearing it.
+ * @see UpdateZonePartialEnclosedPacket To send multiple updates to a zone after clearing it.
+ *
+ * @property localX The X location of this zone relative to the location of the client player.
+ * @property localZ The Z location of this zone relative to the location of the client player.
  */
 data class UpdateZoneFullFollowsPacket(
     val localX: Int,
