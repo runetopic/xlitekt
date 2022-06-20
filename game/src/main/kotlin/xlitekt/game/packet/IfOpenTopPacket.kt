b3f538c9aea40a4f43432bc0ba4be92d0807bff9
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 22:23:09 -0400

    Document IfOpenTopPacket.

--- game/src/main/kotlin/xlitekt/game/packet/IfOpenTopPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/IfOpenTopPacket.kt
@@ -2,6 +2,18 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * <b>Information</b>
+ *
+ * Represents the IF_OPENTOP server -> client packet.
+ *
+ * This packet is used to open a top level interface.
+ *
+ * <b>Assembly Example</b>
+ *
+ * ```
+ * writeShort(interfaceId)
+ * ```
  */
 data class IfOpenTopPacket(
     val interfaceId: Int
