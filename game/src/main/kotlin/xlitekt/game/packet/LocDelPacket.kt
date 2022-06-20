1652e4fd2b23fcc47015f3d4411ccfdea8f6afec
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 20:08:58 -0400

    Document LocDelPacket.

--- game/src/main/kotlin/xlitekt/game/packet/LocDelPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/LocDelPacket.kt
@@ -2,8 +2,15 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * Represents the LOC_DEL server -> client packet.
+ * This packet is used to delete a loc from a zone.
+ *
+ * @property shape The shape of the loc.
+ * @property rotation The rotation of the loc.
+ * @property packedOffset The packed offset location of the loc relative to the client player location.
  */
-class LocDelPacket(
+data class LocDelPacket(
     val shape: Int,
     val rotation: Int,
     val packedOffset: Int
