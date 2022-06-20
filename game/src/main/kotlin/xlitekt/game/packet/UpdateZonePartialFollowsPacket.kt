f4ed679455489d410b8f054b2367768f27e8fa38
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 20:10:27 -0400

    Document UpdateZonePartialFollowsPacket.

--- game/src/main/kotlin/xlitekt/game/packet/UpdateZonePartialFollowsPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/UpdateZonePartialFollowsPacket.kt
@@ -2,6 +2,22 @@ package xlitekt.game.packet
 
 /**
  * @author Jordan Abraham
+ *
+ * Represents the UPDATE_ZONE_PARTIAL_FOLLOWS server -> client packet.
+ * This packet is used to prepare the client to update a single specific zone.
+ * The client takes an input X and Z location and sets temporary field values in code
+ * which is next used by the following packets to update the zone.
+ *
+ * @see ObjAddPacket To add a obj to a zone.
+ * @see ObjDelPacket To delete a obj from a zone.
+ * @see LocAddPacket To add a loc to a zone.
+ * @see LocDelPacket To delete a loc from a zone.
+ * @see MapProjAnimPacket To add a mapProjAnim to a zone.
+ *
+ * @see UpdateZonePartialEnclosedPacket To send multiple updates to a zone instead of a single update.
+ *
+ * @property localX The X location of this zone relative to the location of the client player.
+ * @property localZ The Z location of this zone relative to the location of the client player.
  */
 data class UpdateZonePartialFollowsPacket(
     val localX: Int,
