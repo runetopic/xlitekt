3419fd6c271284fa2c9e00c73123d47c649d7e5b
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 22:32:04 -0400

    Document IfOpenSubPacket.

--- game/src/main/kotlin/xlitekt/game/packet/IfOpenSubPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/IfOpenSubPacket.kt
@@ -2,6 +2,26 @@ package xlitekt.game.packet
 
 /**
  * @author Tyler Telis
+ *
+ * <b>Information</b>
+ *
+ * Represents the IF_OPENSUB server -> client packet.
+ *
+ * This packet is used to open a sub level interface. A sub level interface is that of which
+ * is put onto a top level interface. An example of this is displaying the chat box interface
+ * onto the fixed game mode top level interface.
+ *
+ * <b>Assembly Example</b>
+ *
+ * ```
+ * writeShortLittleEndian(interfaceId)
+ * writeByteSubtract(if (walkable) 1 else 0)
+ * writeInt(toPackedInterface)
+ * ```
+ *
+ * @property interfaceId The interface ID to use for the sub level interface.
+ * @property toPackedInterface The top level packed interface to put this sub interface onto.
+ * @property walkable True if this interface stays open while the client player is walking.
  */
 data class IfOpenSubPacket(
     val interfaceId: Int,
