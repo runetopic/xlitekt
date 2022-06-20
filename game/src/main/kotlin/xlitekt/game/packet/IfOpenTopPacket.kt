bb9287aeb5b97458eeae2ca8666cdb32981de3cb
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 22:31:51 -0400

    Update documentation on IfOpenTopPacket.

--- game/src/main/kotlin/xlitekt/game/packet/IfOpenTopPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/IfOpenTopPacket.kt
@@ -7,13 +7,17 @@ package xlitekt.game.packet
  *
  * Represents the IF_OPENTOP server -> client packet.
  *
- * This packet is used to open a top level interface.
+ * This packet is used to open a top level interface. A top level interface is that of which
+ * is dependent on the client resizeable mode. The top level interface is used as a base to
+ * put sub level interfaces onto.
  *
  * <b>Assembly Example</b>
  *
  * ```
  * writeShort(interfaceId)
  * ```
+ *
+ * @property interfaceId The interface ID to use for the top level interface.
  */
 data class IfOpenTopPacket(
     val interfaceId: Int
