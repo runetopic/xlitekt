5c2c6b8b0a0b46a5e4c2d8a1d634f016e1e3e9e4
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 22:10:08 -0400

    Document CloseModalPacket.

--- game/src/main/kotlin/xlitekt/game/packet/CloseModalPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/CloseModalPacket.kt
@@ -1,3 +1,17 @@
 package xlitekt.game.packet
 
+/**
+ * @author Jordan Abraham
+ *
+ * <b>Information</b>
+ *
+ * Represents the CLOSE_MODAL client -> server packet.
+ *
+ * This packet is used to handle when the client closes a modal. The modal is the
+ * interface that is open in the center of the client that is heavily interacted with.
+ *
+ * <b>Disassembly Exmaple</b>
+ *
+ * This packet has an empty body.
+ */
 class CloseModalPacket : Packet
