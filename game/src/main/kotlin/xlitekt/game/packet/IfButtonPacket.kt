3345298776ac05943c74e444f08289aa2647a71d
Jordan <personal.jordanabraham@gmail.com>
2022-06-19 20:08:31 -0400

    Document IfButtonPacket.

--- game/src/main/kotlin/xlitekt/game/packet/IfButtonPacket.kt
+++ game/src/main/kotlin/xlitekt/game/packet/IfButtonPacket.kt
@@ -3,7 +3,9 @@ package xlitekt.game.packet
 /**
  * @author Jordan Abraham
  *
- * Represents an IF_BUTTON client -> server packet.
+ * Represents the IF_BUTTON client -> server packet.
+ * This packet is used to handle when the client clicks on an interface.
+ *
  * @property index The index of this packet followed by IF_BUTTON. Examples: IF_BUTTON1, IF_BUTTON2.
  * @property packedInterface The packed interface that was clicked by the client. (interfaceId shl 16 or childId).
  * @property slotId The slotId clicked by the client. This defaults to 65535 if there was no container slot clicked.
